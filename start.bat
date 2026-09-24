dfffb67975354ecd17d6580aff42ab9e4ea40ca7
maxmudka94-ai <maxmudka_94@mail.ru>
2026-09-24 22:48:44 +0500

    Delete start.bat

--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Запуск wardogs-monitor: pythonw в фоне, полный путь (как требует сторож-логика).
-cd /d "%~dp0"
-set "PY=C:\Users\Admin\AppData\Local\Programs\Python\Python312\pythonw.exe"
-if not exist "%PY%" set "PY=pythonw.exe"
-start "" "%PY%" "%~dp0bot.py"
